cb3bfbfaa117aec4e135cc4aa1f44e98de408371
Himanshu <h.sahni1998@gmail.com>
2024-06-11 10:56:56 +0200

    added the utils functions

---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,119 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from mlProject import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """
+    Reads a YAML file and returns its content as a ConfigBox object.
+
+    Args:
+        path_to_yaml (Path): Path to the YAML file.
+
+    Raises:
+        ValueError: If the YAML file is empty.
+        Exception: For any other exceptions that occur.
+
+    Returns:
+        ConfigBox: The content of the YAML file as a ConfigBox object.
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("YAML file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """
+    Creates a list of directories.
+
+    Args:
+        path_to_directories (list): List of paths for directories to be created.
+        verbose (bool, optional): If True, logs the creation of each directory. Defaults to True.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """
+    Saves a dictionary as a JSON file.
+
+    Args:
+        path (Path): Path to the JSON file.
+        data (dict): Data to be saved in the JSON file.
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"JSON file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """
+    Loads data from a JSON file.
+
+    Args:
+        path (Path): Path to the JSON file.
+
+    Returns:
+        ConfigBox: The content of the JSON file as a ConfigBox object.
+    """
+    with open(path) as f:
+        content = json.load(f)
+    logger.info(f"JSON file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """
+    Saves data to a binary file.
+
+    Args:
+        data (Any): Data to be saved as binary.
+        path (Path): Path to the binary file.
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"Binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """
+    Loads data from a binary file.
+
+    Args:
+        path (Path): Path to the binary file.
+
+    Returns:
+        Any: Object stored in the file.
+    """
+    data = joblib.load(path)
+    logger.info(f"Binary file loaded from: {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """
+    Gets the size of a file in kilobytes (KB).
+
+    Args:
+        path (Path): Path of the file.
+
+    Returns:
+        str: Size of the file in KB, rounded to the nearest whole number.
+    """
+    size_in_kb = round(os.path.getsize(path) / 1024)
+    return f"~ {size_in_kb} KB"
